f92fd4ad34afa85480b60d35c4ac2a87b93621cb
KingoYong <365501916@qq.com>
2019-07-21 23:22:38 +0800

    okokokok

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -40,7 +41,8 @@ public class AuthorizeController {
     public String callback(@RequestParam("code") String code,
                            @RequestParam("state") String state,
                            //HttpServletRequest里面到底是什么？？
-                           HttpServletRequest request) {
+                           //HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -51,14 +53,16 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             //重定向到index页面，地址栏也会变，如果直接return index则地址栏不会变
-            request.getSession().setAttribute("user",githubUser);
+            //request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
             //登录成功，写cookie和session
 
--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.majiang.community.community.mapper;
 import life.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,4 +17,7 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    //如果形参是类就自动把参数放进#{}里面，如果不是类就要加入注解
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
