2f3f7e1fdf358ecf86818d783b58bd08aee8e0f0
KingoYong <365501916@qq.com>
2019-10-28 23:10:21 +0800

    加入事务，保证插入评论和评论数增加1是一个事务

--- src/main/java/life/majiang/community/community/service/CommentService.java
+++ src/main/java/life/majiang/community/community/service/CommentService.java
@@ -9,6 +9,7 @@ import life.majiang.community.community.mapper.QuestionMapper;
 import life.majiang.community.community.model.Comment;
 import life.majiang.community.community.model.Question;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -25,7 +26,7 @@ public class CommentService {
     private QuestionMapper questionMapper;
     @Resource
     private QuestionExtMapper questionExtMapper;
-
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrCode.TARGET_PARAM_NOT_FOUND);
@@ -39,17 +40,16 @@ public class CommentService {
             if (dbComment == null) {
                 throw new CustomizeException(CustomizeErrCode.COMMENT_NOT_FOUND);
             }
+            commentMapper.insert(comment);
         } else {
             //回复问题
             Question question = questionMapper.selectByPrimaryKey(comment.getParentId());
             if (question == null) {
                 throw new CustomizeException(CustomizeErrCode.QUESTION_ERR);
             }
-            Question updateQuestion = new Question();
-            updateQuestion.setId(comment.getParentId());
-            updateQuestion.setCommentCount(1);
-            questionExtMapper.incCommentCount(updateQuestion);
+            commentMapper.insert(comment);
+            question.setCommentCount(1);
+            questionExtMapper.incCommentCount(question);
         }
-        commentMapper.insert(comment);
     }
 }
